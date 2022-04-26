540f470f3bd45017e7de268dd36c2f7c4f00f2c9
Jordan Momper <jordan.momper.simplon@gmail.com>
2022-04-26 02:06:36 +0200

    did not find a solution for infinite scrolling

--- components/AnimeByGenre.vue
+++ components/AnimeByGenre.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="row">
+    <div class="row" v-if="Page.media">
       <div
         class="col-12 col-sm-6 col-md-4 col-xl-2"
         v-for="el in Page.media"
@@ -17,6 +17,10 @@
         </span>
       </div>
     </div>
+    <div class="actions">
+      <button v-if="showMoreEnabled" @click="showMore">Show more</button>
+    </div>
+
     <!-- {{ Page }} -->
   </div>
 </template>
@@ -26,6 +30,16 @@ import gql from "graphql-tag";
 import AnimeByGenre from "../apollo/queries/AnimeByGenre";
 
 export default {
+  data() {
+    return {
+      Page: {
+        media: [],
+      },
+      page: 0,
+      pageSize: 12,
+      showMoreEnabled: true,
+    };
+  },
   props: {
     genre: String,
   },
@@ -35,21 +49,42 @@ export default {
       variables() {
         return {
           genre: this.genre,
-          perPage: 20,
+          page: this.page,
+          perPage: this.pageSize,
         };
       },
     },
   },
-  data() {
-    return {
-      Page: {
-        media: [],
-      },
-    };
-  },
   methods: {
-    typeOfVar: function (variable) {
-      return Object.prototype.toString.apply(variable);
+    showMore() {
+      this.page++;
+      console.log(this.page);
+      // Fetch more data and transform the original result
+
+      this.$apollo.queries.Page.fetchMore({
+        // New variables
+        variables: {
+          page: this.page,
+        },
+        // Transform the previous result with new data
+        updateQuery: (previousResult, { fetchMoreResult }) => {
+          const newMedias = fetchMoreResult.Page.media;
+          const newPageInfos = fetchMoreResult.Page.pageInfo;
+          const hasMore = fetchMoreResult.Page.hasMore;
+
+          this.showMoreEnabled = hasMore;
+
+          return {
+            Page: {
+              __typename: previousResult.Page.__typename,
+              // Merging the tag list
+              media: [...previousResult.Page.media, ...newMedias],
+              pageInfo: [...previousResult.Page.pageInfo, ...newPageInfos],
+              hasMore,
+            },
+          };
+        },
+      });
     },
   },
 };
